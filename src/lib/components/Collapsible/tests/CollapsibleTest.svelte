06487a1ca6ebee1562a318f13f7b7d70be43e155
ubermanu <manu@paca.happy-dev.fr>
2023-04-30 18:16:26 +0200

    feat: use triggerEvents directly

--- src/lib/components/Collapsible/tests/CollapsibleTest.svelte
+++ src/lib/components/Collapsible/tests/CollapsibleTest.svelte
@@ -4,17 +4,11 @@
   export let defaults = {}
 
   const provider = createCollapsibleProvider(defaults)
-  const { triggerProps, contentProps } = provider
+  const { triggerEvents, triggerProps, contentProps } = provider
 </script>
 
 <div data-testid="collapsible">
-  <div
-    data-testid="trigger"
-    on:click={() => provider.toggle()}
-    {...$triggerProps}
-  >
-    Trigger
-  </div>
+  <div data-testid="trigger" use:triggerEvents {...$triggerProps}>Trigger</div>
   <div data-testid="content" {...$contentProps}>
     <p>Content</p>
   </div>
