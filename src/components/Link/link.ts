9109795dba68459386870036047f4e495e0327ef
ubermanu <manu@paca.happy-dev.fr>
2023-05-17 13:12:19 +0200

    feat: link

---
+++ src/components/Link/link.ts
@@ -0,0 +1,79 @@
+import type { Action } from 'svelte/action'
+import { derived, get, writable } from 'svelte/store'
+
+export type LinkConfig = {
+  disabled?: boolean
+  href?: string
+  target?: string
+}
+
+export const createLink = (config: LinkConfig) => {
+  const { disabled, href, target } = {
+    disabled: false,
+    ...config,
+  }
+
+  const disabled$ = writable(disabled || false)
+  const href$ = writable(href || '')
+  const target$ = writable(target || '')
+
+  const linkProps = derived(disabled$, (disabled) => ({
+    role: 'link',
+    'aria-disabled': disabled,
+    tabIndex: disabled ? -1 : 0,
+  }))
+
+  const onLinkClick = (event: MouseEvent) => {
+    event.preventDefault()
+
+    if (get(disabled$)) {
+      return
+    }
+
+    navigate()
+  }
+
+  const onLinkKeyDown = (event: KeyboardEvent) => {
+    if (get(disabled$)) {
+      return
+    }
+
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      navigate()
+    }
+  }
+
+  const navigate = () => {
+    const href = get(href$)
+    const target = get(target$)
+
+    if (href.startsWith('#')) {
+      const element = document.querySelector(href)
+      element?.scrollIntoView()
+      return
+    }
+
+    window.open(href, target)
+  }
+
+  const useLink: Action = (node) => {
+    node.addEventListener('click', onLinkClick)
+    node.addEventListener('keydown', onLinkKeyDown)
+
+    return {
+      destroy() {
+        node.removeEventListener('click', onLinkClick)
+        node.removeEventListener('keydown', onLinkKeyDown)
+      },
+    }
+  }
+
+  return {
+    href: href$,
+    target: target$,
+    disabled: disabled$,
+    linkProps,
+    useLink,
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './components/Accordion/accordion.js'
 export * from './components/Button/button.js'
 export * from './components/Collapsible/collapsible.js'
+export * from './components/Link/link.js'
 export * from './components/ToggleButton/toggleButton.js'
