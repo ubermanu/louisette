e46b37b7f902c75be41ace44c39f729e5b80f33f
ubermanu <manu@paca.happy-dev.fr>
2023-05-17 16:33:36 +0200

    feat: export cal

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './components/Accordion/accordion.js'
 export * from './components/Button/button.js'
+export * from './components/Calendar/calendar.js'
 export * from './components/Collapsible/collapsible.js'
 export * from './components/Link/link.js'
 export * from './components/ToggleButton/toggleButton.js'
