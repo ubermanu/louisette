9ed011d297b7fb0d58dcf76ebd3fd05f9b5655ca
ubermanu <manu@paca.happy-dev.fr>
2023-05-19 18:42:48 +0200

    feat: hover interaction

---
+++ src/interactions/Hover/hover.ts
@@ -0,0 +1,61 @@
+import type { Action } from 'svelte/action'
+import { readonly, writable } from 'svelte/store'
+
+export type HoverEvent = {
+  type: 'hoverstart' | 'hoverend'
+  pointerType: PointerEvent['pointerType']
+  target: EventTarget | null
+}
+
+export type HoverConfig = {
+  onHoverStart?: (event: HoverEvent) => void
+  onHoverEnd?: (event: HoverEvent) => void
+}
+
+const useHover = (config?: HoverConfig) => {
+  const { onHoverStart, onHoverEnd } = {
+    ...config,
+  }
+
+  const hovering$ = writable(false)
+
+  const onPointerEnter = (event: PointerEvent) => {
+    hovering$.set(true)
+    onHoverStart?.({
+      type: 'hoverstart',
+      pointerType: event.pointerType,
+      target: event.target,
+    })
+  }
+
+  const onPointerLeave = (event: PointerEvent) => {
+    hovering$.set(false)
+    onHoverEnd?.({
+      type: 'hoverend',
+      pointerType: event.pointerType,
+      target: event.target,
+    })
+  }
+
+  const hoverRef: Action = (node) => {
+    if (onHoverStart) {
+      node.addEventListener('pointerenter', onPointerEnter)
+    }
+
+    if (onHoverEnd) {
+      node.addEventListener('pointerleave', onPointerLeave)
+    }
+
+    return {
+      destroy() {
+        node.removeEventListener('pointerenter', onPointerEnter)
+        node.removeEventListener('pointerleave', onPointerLeave)
+      },
+    }
+  }
+
+  return {
+    hovering: readonly(hovering$),
+    hoverRef,
+  }
+}
