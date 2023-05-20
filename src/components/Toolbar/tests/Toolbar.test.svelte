a95ddc08ea992a5f1dbe420dbfc2bea57266ce15
ubermanu <manu@paca.happy-dev.fr>
2023-05-20 11:52:18 +0200

    feat: toolbar (wip)

---
+++ src/components/Toolbar/tests/Toolbar.test.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { createToolbar } from '../toolbar.js'
+
+  const { useToolbar, toolbarProps, itemProps } = createToolbar()
+</script>
+
+<div use:useToolbar {...$toolbarProps} data-testid="toolbar">
+  <button {...$itemProps('bold')} data-testid="bold">B</button>
+  <button {...$itemProps('italic')} data-testid="italic">I</button>
+  <button {...$itemProps('underline')} data-testid="underline">U</button>
+</div>
---
+++ src/components/Toolbar/toolbar.ts
@@ -0,0 +1,94 @@
+import { delegate } from '$lib/helpers.js'
+import type { Action } from 'svelte/action'
+import { derived, get, readable, writable } from 'svelte/store'
+
+export type ToolbarConfig = {
+  orientation?: 'horizontal' | 'vertical'
+}
+
+export const createToolbar = (config?: ToolbarConfig) => {
+  const { orientation } = { ...config }
+
+  const orientation$ = writable(orientation || 'horizontal')
+  const focused$ = writable('')
+
+  const toolbarProps = derived([orientation$], ([orientation]) => ({
+    role: 'toolbar',
+    'aria-orientation': orientation,
+  }))
+
+  const itemProps = readable((key: string) => ({
+    'data-toolbar-item': key,
+    tabIndex: -1,
+    inert: '',
+  }))
+
+  let rootNode: HTMLElement | null = null
+
+  const onItemKeyDown = (event: KeyboardEvent) => {
+    const currentTarget = event.target as HTMLElement
+
+    const items = rootNode?.querySelectorAll(
+      '[data-toolbar-item]'
+    ) as NodeListOf<HTMLElement>
+    if (!items) return
+
+    const currentIndex = Array.from(items).indexOf(currentTarget)
+    if (currentIndex === -1) return
+
+    const $orientation = get(orientation$)
+
+    if (
+      (event.key === 'ArrowLeft' && $orientation === 'horizontal') ||
+      (event.key === 'ArrowUp' && $orientation === 'vertical')
+    ) {
+      event.preventDefault()
+      const prevIndex = currentIndex === 0 ? items.length - 1 : currentIndex - 1
+      items[prevIndex]?.focus()
+    }
+
+    if (
+      (event.key === 'ArrowRight' && $orientation === 'horizontal') ||
+      (event.key === 'ArrowDown' && $orientation === 'vertical')
+    ) {
+      event.preventDefault()
+      const nextIndex = currentIndex === items.length - 1 ? 0 : currentIndex + 1
+      items[nextIndex]?.focus()
+    }
+
+    if (event.key === 'Home') {
+      event.preventDefault()
+      items[0]?.focus()
+    }
+
+    if (event.key === 'End') {
+      event.preventDefault()
+      items[items.length - 1]?.focus()
+    }
+  }
+
+  const useToolbar: Action = (node) => {
+    rootNode = node
+
+    const events = {
+      keydown: {
+        '[data-toolbar-item]': onItemKeyDown,
+      },
+    }
+
+    const removeListeners = delegate(node, events)
+
+    return {
+      destroy() {
+        removeListeners()
+      },
+    }
+  }
+
+  return {
+    orientation: orientation$,
+    toolbarProps,
+    itemProps,
+    useToolbar,
+  }
+}
