3afba7eeb81de60fa16d418def0e4b10c4627147
ubermanu <manu@paca.happy-dev.fr>
2023-05-20 16:00:10 +0200

    feat: tooltip (wip)

---
+++ src/components/Tooltip/test/Tooltip.test.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { createTooltip } from '../tooltip.js'
+
+  const { triggerEvents, triggerProps, tooltipProps } = createTooltip()
+</script>
+
+<div use:triggerEvents {...$triggerProps}>Hover me!</div>
+
+<div {...$tooltipProps}>The content of the tooltip</div>
---
+++ src/components/Tooltip/tooltip.ts
@@ -0,0 +1,75 @@
+import { generateId } from '$lib/helpers.js'
+import type { Action } from 'svelte/action'
+import { derived, readable, readonly, writable } from 'svelte/store'
+
+export const createTooltip = () => {
+  const tooltipId = generateId()
+  const opened$ = writable(false)
+
+  const tooltipProps = derived(opened$, (opened) => ({
+    id: tooltipId,
+    role: 'tooltip',
+    inert: !opened,
+    'aria-hidden': !opened ? 'true' : undefined,
+  }))
+
+  const triggerProps = readable({
+    'aria-describedby': tooltipId,
+  })
+
+  const open = () => {
+    opened$.set(true)
+  }
+
+  const close = () => {
+    opened$.set(false)
+  }
+
+  const onTriggerPointerEnter = (event: PointerEvent) => {
+    open()
+    event.currentTarget?.addEventListener('pointerleave', onTriggerPointerLeave)
+  }
+
+  const onTriggerPointerLeave = () => {
+    close()
+  }
+
+  const onTriggerKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      close()
+    }
+  }
+
+  const onTriggerFocus = (event: FocusEvent) => {
+    open()
+    event.currentTarget?.addEventListener('blur', onTriggerBlur)
+  }
+
+  const onTriggerBlur = () => {
+    close()
+  }
+
+  // TODO: The tooltip should keep open when the mouse is over the tooltip
+  const triggerEvents: Action = (node) => {
+    node.addEventListener('pointerenter', onTriggerPointerEnter)
+    node.addEventListener('keydown', onTriggerKeyDown)
+    node.addEventListener('focus', onTriggerFocus)
+
+    return {
+      destroy() {
+        node.removeEventListener('pointerenter', onTriggerPointerEnter)
+        node.removeEventListener('keydown', onTriggerKeyDown)
+        node.removeEventListener('focus', onTriggerFocus)
+      },
+    }
+  }
+
+  return {
+    opened: readonly(opened$),
+    tooltipProps,
+    triggerProps,
+    triggerEvents,
+    open,
+    close,
+  }
+}
