a3131137b3426add3aefa7e3ac02c4f0fe2a356b
ubermanu <manu@paca.happy-dev.fr>
2023-05-24 14:23:49 +0200

    chore: link example

---
+++ src/routes/component/link/example/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { createLink } from '$lib'
+  import { page } from '$app/stores'
+
+  const { link, linkAttrs } = createLink({
+    href: $page.url.pathname,
+  })
+</script>
+
+<img
+  use:link
+  {...$linkAttrs}
+  src="https://placekitten.com/200/300"
+  alt="random image"
+  class="m-2 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+/>
