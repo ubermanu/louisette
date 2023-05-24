3b9661bc742252fadb7828581790c915faf51a16
ubermanu <manu@paca.happy-dev.fr>
2023-05-24 14:24:38 +0200

    chore: update img alt

--- src/routes/component/link/example/+page.svelte
+++ src/routes/component/link/example/+page.svelte
@@ -11,6 +11,6 @@
   use:link
   {...$linkAttrs}
   src="https://placekitten.com/200/300"
-  alt="random image"
+  alt="An image of a kitten"
   class="m-2 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
 />
