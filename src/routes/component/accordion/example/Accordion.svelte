fe04226a8cdedc826de8985837e026d3d0227304
ubermanu <manu@paca.happy-dev.fr>
2023-05-24 12:10:37 +0200

    chore: accordion example

---
+++ src/routes/component/accordion/example/Accordion.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { createAccordion } from '$lib'
+  import { setContext } from 'svelte'
+
+  const { accordion, ...accordionContext } = createAccordion()
+  setContext('accordion', accordionContext)
+</script>
+
+<div use:accordion>
+  <slot />
+</div>
