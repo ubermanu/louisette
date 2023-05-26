fae21ab3b32d43dd2d055e9b9c06da89a703578e
ubermanu <manu@paca.happy-dev.fr>
2023-05-26 09:42:55 +0200

    fix: button active style

--- src/routes/component/button/example/Button.svelte
+++ src/routes/component/button/example/Button.svelte
@@ -7,7 +7,7 @@
 <div
   use:button
   {...$buttonAttrs}
-  class="inline-flex w-auto select-none items-center rounded-md border border-transparent bg-accent-500 px-4 py-2 text-base font-medium text-white shadow-sm transition duration-150 ease-in-out hover:bg-accent-400 focus:outline-none focus-visible:ring focus-visible:ring-accent-500 focus-visible:ring-opacity-50 active:bg-accent-500 active:shadow-inner"
+  class="inline-flex w-auto select-none items-center rounded-md border border-transparent bg-accent-500 px-4 py-2 text-base font-medium text-white shadow-sm transition duration-150 ease-in-out hover:bg-accent-400 focus:outline-none focus-visible:ring focus-visible:ring-accent-500 focus-visible:ring-opacity-50 active:bg-accent-700 active:shadow-inner"
   on:click
   on:keydown
   on:keypress
