a12218a2efcf70935d4b46933a512fe4347d5724
ubermanu <manu@paca.happy-dev.fr>
2023-05-26 10:15:24 +0200

    docs: image link focus opacity

--- src/routes/component/link/example/+page.svelte
+++ src/routes/component/link/example/+page.svelte
@@ -13,5 +13,5 @@
   {...$linkAttrs}
   src="https://placekitten.com/200/300"
   alt="An image of a kitten"
-  class="w-[10rem] cursor-pointer rounded-sm focus:outline-none focus-visible:ring focus-visible:ring-accent-500"
+  class="w-[10rem] cursor-pointer rounded-sm focus:outline-none focus-visible:ring focus-visible:ring-accent-500 focus-visible:ring-opacity-50"
 />
