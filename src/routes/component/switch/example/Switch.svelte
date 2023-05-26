c38c08a7152cd7f464d84a1d3a849a69a74e54d3
ubermanu <manu@paca.happy-dev.fr>
2023-05-26 10:18:39 +0200

    docs: update switch example

--- src/routes/component/switch/example/Switch.svelte
+++ src/routes/component/switch/example/Switch.svelte
@@ -7,17 +7,17 @@
 <div
   use:switchRef
   {...$switchAttrs}
-  class="flex h-6 w-12 cursor-pointer items-center rounded-full border border-neutral-300 bg-white text-neutral-900 shadow-inner transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-50 dark:border-neutral-600 dark:bg-neutral-800 dark:text-neutral-100"
+  class="flex h-6 w-12 cursor-pointer items-center rounded-full border border-neutral-300 bg-white text-neutral-900 shadow-inner transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring focus-visible:ring-accent-500 focus-visible:ring-opacity-50 dark:border-neutral-600 dark:bg-neutral-800 dark:text-neutral-100"
   class:is-active={$active}
 >
   <div
     class="transition-margin mx-1 h-4 w-4 rounded-full bg-neutral-700 shadow-md duration-300 ease-in-out dark:bg-neutral-300"
-    class:ml-auto={$active}
+    class:translate-x-6={$active}
   />
 </div>
 
 <style lang="postcss">
   .is-active {
-    @apply border-accent-100 bg-accent-100 text-accent-700 dark:border-accent-700 dark:bg-accent-700 dark:text-accent-100;
+    @apply border-accent-300 bg-accent-300 text-accent-700 dark:border-accent-600 dark:bg-accent-600 dark:text-accent-100;
   }
 </style>
