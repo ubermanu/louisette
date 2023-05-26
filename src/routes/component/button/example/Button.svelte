2461e4c041e24ea51868f2607c0b85fee2b820c9
ubermanu <manu@paca.happy-dev.fr>
2023-05-26 09:38:13 +0200

    fix: button styles + code tag in blockquotes

--- src/routes/component/button/example/Button.svelte
+++ src/routes/component/button/example/Button.svelte
@@ -7,7 +7,7 @@
 <div
   use:button
   {...$buttonAttrs}
-  class="inline-flex w-auto select-none items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+  class="inline-flex w-auto select-none items-center rounded-md border border-transparent bg-accent-500 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-accent-400 focus:outline-none focus-visible:ring focus-visible:ring-accent-500 focus-visible:ring-opacity-50"
   on:click
   on:keydown
   on:keypress
