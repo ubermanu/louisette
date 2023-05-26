3973ee262f294a809a48ac1cd303ca1d2fab2ce4
ubermanu <manu@paca.happy-dev.fr>
2023-05-26 09:40:51 +0200

    feat: active styles on buttons

--- src/routes/component/toggle-button/example/ToggleButton.svelte
+++ src/routes/component/toggle-button/example/ToggleButton.svelte
@@ -9,7 +9,7 @@
   use:button
   {...$buttonAttrs}
   class:is-checked={$checked}
-  class="inline-flex w-auto select-none items-center gap-2 rounded-md bg-accent px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-accent-400 focus-visible:ring focus-visible:ring-accent focus-visible:ring-opacity-50 dark:hover:bg-accent-400"
+  class="inline-flex w-auto select-none items-center gap-2 rounded-md bg-accent px-4 py-2 text-base font-medium text-white shadow-sm transition duration-150 ease-in-out hover:bg-accent-400 focus-visible:ring focus-visible:ring-accent focus-visible:ring-opacity-50 active:bg-accent-700 active:shadow-inner dark:hover:bg-accent-400 dark:focus-visible:ring-accent-400 dark:focus-visible:ring-opacity-50 dark:active:bg-accent-700"
   on:click
   on:keydown
   on:keypress
@@ -21,6 +21,6 @@
 
 <style lang="postcss">
   .is-checked {
-    @apply bg-green-700 hover:bg-green-600 dark:bg-green-700 dark:hover:bg-green-600;
+    @apply bg-green-700 hover:bg-green-600 active:bg-green-800 dark:bg-green-700 dark:hover:bg-green-600 dark:active:bg-green-800;
   }
 </style>
