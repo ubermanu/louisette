fa14c2851611867e39cb46db9f522a97beb22a5a
ubermanu <manu@paca.happy-dev.fr>
2023-05-27 16:50:01 +0200

    feat: copy to clipboard

--- src/routes/Snippet.svelte
+++ src/routes/Snippet.svelte
@@ -1,7 +1,29 @@
 <script lang="ts">
+  import { Check, Clipboard } from 'lucide-svelte'
   import { HighlightSvelte } from 'svelte-highlight'
 
   export let code: string
+
+  let copied: boolean = false
+  let timeout: number
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(code?.replace('$lib', 'louisette'))
+    copied = true
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      copied = false
+    }, 1000)
+  }
 </script>
 
-<HighlightSvelte code={code?.replace('$lib', 'louisette')} />
+<div class="relative block w-full">
+  <HighlightSvelte code={code?.replace('$lib', 'louisette')} />
+  <button
+    on:click={copyToClipboard}
+    class="absolute right-2 top-2 rounded p-2 transition-colors duration-200 hover:bg-neutral-200 dark:hover:bg-neutral-800"
+    title={copied ? 'Copied!' : 'Copy to clipboard'}
+  >
+    <svelte:component this={copied ? Check : Clipboard} class="h-5 w-5" />
+  </button>
+</div>
