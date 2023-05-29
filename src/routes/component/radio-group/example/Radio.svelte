faa117c78be35f72c0ad47bd367b17dd2429c9ad
ubermanu <manu@paca.happy-dev.fr>
2023-05-29 17:05:16 +0200

    carry the intent of a radio in the group example

--- src/routes/component/radio-group/example/Radio.svelte
+++ src/routes/component/radio-group/example/Radio.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { RadioGroup } from '$lib'
   import { getContext } from 'svelte'
+  import { CheckCircle, Circle } from 'lucide-svelte'
 
   export let value: string
 
@@ -9,10 +10,16 @@
 
 <div
   {...$radioAttrs(value)}
-  class="cursor-pointer rounded-md p-4 text-neutral-900 transition-colors hover:bg-neutral-200 focus:outline-none focus-visible:bg-neutral-200 focus-visible:ring focus-visible:ring-accent-500 focus-visible:ring-opacity-50 dark:border-neutral-600 dark:text-neutral-100 dark:hover:bg-neutral-700 dark:focus-visible:bg-neutral-700"
+  class="flex cursor-pointer items-center rounded-md p-4 text-neutral-900 transition-colors hover:bg-neutral-200 focus:outline-none focus-visible:bg-neutral-200 focus-visible:ring focus-visible:ring-accent-500 focus-visible:ring-opacity-50 dark:border-neutral-600 dark:text-neutral-100 dark:hover:bg-neutral-700 dark:focus-visible:bg-neutral-700"
   class:is-selected={$selected === value}
 >
-  <slot />
+  <svelte:component
+    this={$selected === value ? CheckCircle : Circle}
+    class="mr-4 h-4 w-4 flex-shrink-0"
+  />
+  <span>
+    <slot />
+  </span>
 </div>
 
 <style lang="postcss">
