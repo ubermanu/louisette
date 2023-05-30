83d5136484b63e0f2e99fc3a813892dde4ab740d
ubermanu <manu@paca.happy-dev.fr>
2023-05-30 18:42:50 +0200

    fix: header for very small viewports

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -8,13 +8,13 @@
 
 <div class="container mx-auto flex max-w-6xl items-center gap-4 p-4">
   <a class="mr-auto flex items-center gap-2 rounded p-2" href="{base}/">
-    <img src={logo} alt class="h-9 w-9" />
-    <span class="font-serif text-3xl">Louisette</span>
+    <img src={logo} alt class="h-9 w-9 max-sm:hidden" />
+    <span class="font-serif text-2xl md:text-3xl">Louisette</span>
     <span class="ml-2 rounded bg-accent-400 p-1 text-xs text-accent-900">
       {LOUISETTE_VERSION}
     </span>
   </a>
-  <div class="flex gap-2 md:gap-4">
+  <div class="flex gap-2 max-sm:gap-1 md:gap-4">
     <a
       href="https://github.com/ubermanu/louisette"
       target="_blank"
