bf523993bb5d614f0a7833af66d68021cf5004b5
ubermanu <manu@paca.happy-dev.fr>
2023-05-30 16:46:33 +0200

    docs: reduce home content size

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <div class="flex h-[60vh] flex-col items-center justify-center">
-  <img src={logo} alt class="h-32 w-32" />
-  <p class="font-serif text-6xl">Louisette</p>
+  <img src={logo} alt class="h-24 w-24 md:h-32 md:w-32" />
+  <p class="font-serif text-5xl md:text-6xl">Louisette</p>
   <p class="text-md px-2 text-center opacity-60">
     <b>Louisette</b> is a collection of component primitives for <b>Svelte</b>.
   </p>
