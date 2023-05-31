bd47a32c1c095a83639475d69c2a3ee168be09af
ubermanu <manu@paca.happy-dev.fr>
2023-05-31 12:24:41 +0200

    test keyboard navigation in toolbar

--- src/lib/components/Toolbar/tests/Toolbar.test.svelte
+++ src/lib/components/Toolbar/tests/Toolbar.test.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import { createToolbar } from '$lib'
 
+  export let defaults = {}
+
   export let items: { id: string; text: string; disabled: boolean }[] = []
 
-  const { toolbar, toolbarAttrs, itemAttrs } = createToolbar()
+  const { toolbar, toolbarAttrs, itemAttrs } = createToolbar(defaults)
 </script>
 
 <div use:toolbar {...$toolbarAttrs} data-testid="toolbar">
--- src/lib/components/Toolbar/tests/Toolbar.test.ts
+++ src/lib/components/Toolbar/tests/Toolbar.test.ts
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/svelte'
+import { fireEvent, render } from '@testing-library/svelte'
 import { describe, expect, test } from 'vitest'
 import ToolbarTest from './Toolbar.test.svelte'
 
@@ -41,5 +41,64 @@ describe('Toolbar', () => {
     expect(getByTestId('toolbar-item-underline').getAttribute('tabindex')).toBe(
       '-1'
     )
+  })
+
+  test('Handles keyboard navigation', async () => {
+    const { getByTestId } = render(ToolbarTest, {
+      props: {
+        items: [
+          { id: 'bold', label: 'B' },
+          { id: 'italic', label: 'I' },
+          { id: 'underline', label: 'U' },
+        ],
+      },
+    })
+
+    await fireEvent.keyDown(getByTestId('toolbar-item-bold'), {
+      key: 'ArrowRight',
+    })
+
+    expect(document.activeElement).toBe(getByTestId('toolbar-item-italic'))
+
+    await fireEvent.keyDown(getByTestId('toolbar-item-italic'), {
+      key: 'ArrowLeft',
+    })
+
+    expect(document.activeElement).toBe(getByTestId('toolbar-item-bold'))
+
+    await fireEvent.keyDown(getByTestId('toolbar-item-bold'), { key: 'End' })
+
+    expect(document.activeElement).toBe(getByTestId('toolbar-item-underline'))
+
+    await fireEvent.keyDown(getByTestId('toolbar-item-underline'), {
+      key: 'Home',
+    })
+
+    expect(document.activeElement).toBe(getByTestId('toolbar-item-bold'))
+  })
+
+  test('Handles keyboard navigation (vertical)', async () => {
+    const { getByTestId } = render(ToolbarTest, {
+      props: {
+        items: [
+          { id: 'bold', label: 'B' },
+          { id: 'italic', label: 'I' },
+          { id: 'underline', label: 'U' },
+        ],
+        defaults: {
+          orientation: 'vertical',
+        },
+      },
+    })
+
+    await fireEvent.keyDown(getByTestId('toolbar-item-bold'), {
+      key: 'ArrowDown',
+    })
+    expect(document.activeElement).toBe(getByTestId('toolbar-item-italic'))
+
+    await fireEvent.keyDown(getByTestId('toolbar-item-italic'), {
+      key: 'ArrowUp',
+    })
+    expect(document.activeElement).toBe(getByTestId('toolbar-item-bold'))
   })
 })
