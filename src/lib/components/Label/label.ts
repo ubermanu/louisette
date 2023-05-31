bf78e886eda531efce0ab41c32c0a29f2ce7ce2f
ubermanu <manu@paca.happy-dev.fr>
2023-05-31 12:55:03 +0200

    feat: label

---
+++ src/lib/components/Label/label.ts
@@ -0,0 +1,38 @@
+import { generateId } from '$lib/helpers/uuid.js'
+import type { Action } from 'svelte/action'
+import { readable } from 'svelte/store'
+
+export const createLabel = () => {
+  const labelId = generateId()
+
+  const labelAttrs = readable({
+    id: labelId,
+  })
+
+  const fieldAttrs = readable({
+    'aria-labelledby': labelId,
+  })
+
+  const onLabelClick = (event: MouseEvent) => {
+    const field = document.querySelector(
+      `[aria-labelledby="${labelId}"]`
+    ) as HTMLElement
+    field?.focus()
+  }
+
+  const useLabel: Action = (node) => {
+    node.addEventListener('click', onLabelClick)
+
+    return {
+      destroy() {
+        node.removeEventListener('click', onLabelClick)
+      },
+    }
+  }
+
+  return {
+    label: useLabel,
+    labelAttrs,
+    fieldAttrs,
+  }
+}
