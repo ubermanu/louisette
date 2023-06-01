fb74a68856e1cf669a77216e3e4b6f9776d3ef1f
ubermanu <manu@paca.happy-dev.fr>
2023-06-01 11:22:43 +0200

    feat: use popover in the menu example for desktop

--- src/routes/(docs)/component/menu/example/MenuItem.svelte
+++ src/routes/(docs)/component/menu/example/MenuItem.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { useHover, useFocusVisible, type Menu } from '$lib'
-  import { getContext } from 'svelte'
+  import { createPopover, useHover, type Menu } from '$lib'
+  import { getContext, tick } from 'svelte'
   import { ChevronRight } from 'lucide-svelte'
 
   export let href: string
@@ -8,31 +8,77 @@
   // Generate a random key for this menu item
   const key = Math.random().toString(36).substring(7)
 
-  const { itemAttrs } = getContext<Menu>('menu')
-
+  // Check if this menu item has a submenu
   const hasSubmenu = $$slots.submenu !== undefined
 
-  const { hover, hovering } = useHover()
-  const { focusVisible, focused } = useFocusVisible()
+  // Get the menu item attributes
+  const { itemAttrs, orientation } = getContext<Menu>('menu')
 
-  // TODO: Add proper menu navigation
-  // TODO: Use popover primitive
+  // Create a popover for the submenu
+  const { triggerAttrs, visible, show, hide } = createPopover()
+
+  // Reveals the submenu when the menu item is hovered
+  const { hover } = useHover({
+    onHoverStart: () => show(),
+    onHoverEnd: () => hide(),
+  })
+
+  let menuitem: HTMLElement
+  let submenuContainer: HTMLElement
+
+  // TODO: Improve keyboard navigation (WIP)
+  const onKeyDown = (event: KeyboardEvent) => {
+    if (!hasSubmenu) {
+      return
+    }
+
+    if (
+      (event.key === 'ArrowRight' && $orientation === 'vertical') ||
+      (event.key === 'ArrowDown' && $orientation === 'horizontal')
+    ) {
+      event.preventDefault()
+      show()
+
+      // Focus the first menu item in the submenu
+      // TODO: Find a way to get the submenu instance?
+      tick().then(() => {
+        ;(
+          submenuContainer.querySelector('[data-menu-item]') as HTMLElement
+        )?.focus()
+      })
+    }
+
+    if (
+      event.key === 'Escape' ||
+      (event.key === 'ArrowLeft' && $orientation === 'vertical') ||
+      (event.key === 'ArrowUp' && $orientation === 'horizontal')
+    ) {
+      event.preventDefault()
+      hide()
+      menuitem?.focus()
+    }
+  }
 </script>
 
-<li class:relative={hasSubmenu} use:hover use:focusVisible>
+<li class="relative" use:hover>
   <a
+    bind:this={menuitem}
     {...$itemAttrs(key)}
+    {...$triggerAttrs}
     {href}
     class="flex items-center justify-between gap-2 rounded-sm px-4 py-1 text-sm hover:bg-neutral-200 dark:hover:bg-neutral-700"
-    aria-haspopup={hasSubmenu ? 'true' : undefined}
+    on:keydown={onKeyDown}
   >
     <span><slot /></span>
     {#if hasSubmenu}
       <ChevronRight class="h-4 w-4" />
     {/if}
   </a>
-  {#if hasSubmenu && ($hovering || $focused)}
-    <div class="absolute left-full top-0 z-10 -mt-2 pl-2 shadow-lg">
+  {#if hasSubmenu && $visible}
+    <div
+      bind:this={submenuContainer}
+      class="absolute left-full top-0 z-10 -mt-2 pl-2 shadow-lg"
+    >
       <slot name="submenu" />
     </div>
   {/if}
