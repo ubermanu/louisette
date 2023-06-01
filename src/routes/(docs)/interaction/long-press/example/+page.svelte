4aa35267b5e5d61bc72771b9687d2e9e99f72939
ubermanu <manu@paca.happy-dev.fr>
2023-06-01 16:50:39 +0200

    docs: long press

---
+++ src/routes/(docs)/interaction/long-press/example/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import ConfettiButton from './ConfettiButton.svelte'
+</script>
+
+<ConfettiButton>Press me longer for confetti!</ConfettiButton>
---
+++ src/routes/(docs)/interaction/long-press/example/ConfettiButton.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { useLongPress } from '$lib'
+  import confetti from 'canvas-confetti'
+
+  let threshold = 1000
+
+  const { longPress, pressed } = useLongPress({
+    threshold,
+    onLongPress: () => confetti(),
+  })
+</script>
+
+<button
+  use:longPress
+  class="inline-flex items-center rounded bg-neutral-200 p-2 shadow-sm hover:bg-neutral-300 dark:bg-neutral-700 dark:hover:bg-neutral-600"
+  class:is-pressed={$pressed}
+>
+  <slot />
+</button>
+
+<style lang="postcss">
+  .is-pressed {
+    @apply scale-95 shadow-none;
+  }
+</style>
