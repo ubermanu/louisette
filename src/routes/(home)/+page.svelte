fbfc371763831cce9b1358f34e563078d60cc730
ubermanu <manu@paca.happy-dev.fr>
2023-06-13 13:42:18 +0200

    fix: spacings

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -11,11 +11,11 @@
 
 <div class="flex h-[60vh] flex-col items-center justify-center">
   <img src={logo} alt class="h-24 w-24 md:h-32 md:w-32" />
-  <p class="font-serif text-5xl md:text-6xl">Louisette</p>
+  <p class="mb-6 font-serif text-5xl md:text-6xl">Louisette</p>
   <p class="text-md px-2 text-center opacity-60">
     <b>Louisette</b> is a collection of component primitives for <b>Svelte</b>.
   </p>
-  <p class="mt-4">
+  <p class="my-4">
     <Highlight language={bash} code="npm install louisette" />
   </p>
   <p>
