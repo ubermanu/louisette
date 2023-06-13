273d3333558435b22423072e9b7a9fab9bed0007
ubermanu <manu@paca.happy-dev.fr>
2023-06-13 13:39:23 +0200

    fix: spacings

--- src/routes/(docs)/[...path]/+page.svelte
+++ src/routes/(docs)/[...path]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Snippet from '../Snippet.svelte'
   import { title } from '../../title.js'
+  import { page } from '$app/stores'
 
   export let data
 
@@ -9,19 +10,21 @@
 
 <div class="prose prose-neutral dark:prose-invert">
   <svelte:component this={data.component} />
+
+  {#if data.example}
+    <h2>Example</h2>
+    <div class="not-prose">
+      <svelte:component this={data.example} />
+    </div>
+  {/if}
+
+  {#if data.sources?.length > 0}
+    <h2>Sources</h2>
+    {#each data.sources as source}
+      <p>{source.filename}</p>
+      <div class="not-prose">
+        <Snippet code={source.code} />
+      </div>
+    {/each}
+  {/if}
 </div>
-
-{#if data.example}
-  <h2>Example</h2>
-  <div>
-    <svelte:component this={data.example} />
-  </div>
-{/if}
-
-{#if data.sources?.length > 0}
-  <h2>Sources</h2>
-  {#each data.sources as source}
-    <p>{source.filename}</p>
-    <Snippet code={source.code} />
-  {/each}
-{/if}
