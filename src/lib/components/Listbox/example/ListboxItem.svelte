58e9ccd33ec871e6fb258c1e0a30d762917981a4
ubermanu <manu@paca.happy-dev.fr>
2023-06-14 15:13:58 +0200

    update styles

--- src/lib/components/Listbox/example/ListboxItem.svelte
+++ src/lib/components/Listbox/example/ListboxItem.svelte
@@ -4,25 +4,39 @@
 
   export let value: string
 
-  const { optionAttrs, selected, activeDescendant } =
-    getContext<Listbox>('listbox')
+  const {
+    optionAttrs,
+    selected: selectedList,
+    activeDescendant,
+  } = getContext<Listbox>('listbox')
 </script>
 
 <div
-  class="cursor-pointer rounded-md p-2 transition-colors hover:bg-neutral-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-500 focus-visible:ring-opacity-50 dark:hover:bg-neutral-700"
-  class:is-selected={$selected.includes(value)}
+  class="flex cursor-pointer items-center justify-between gap-4 rounded-md p-2 transition-colors hover:bg-neutral-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-500 focus-visible:ring-opacity-50 dark:hover:bg-neutral-700"
   class:is-active-descendant={$activeDescendant === value}
   {...$optionAttrs(value)}
 >
-  <slot />
+  <span>
+    <slot />
+  </span>
+  {#if $selectedList.includes(value)}
+    <svg
+      class="text-primary-500 dark:text-primary-400 ml-2 h-4 w-4"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      aria-hidden="true"
+    >
+      <path
+        clip-rule="evenodd"
+        d="M10.707 14.707a1 1 0 01-1.414 0L5 10.414A1 1 0 016.414 9l3.293 3.293 6.293-6.293a1 1 0 111.414 1.414l-7 7z"
+        fill-rule="evenodd"
+      />
+    </svg>
+  {/if}
 </div>
 
 <style lang="postcss">
-  .is-selected {
-    @apply bg-accent-200 dark:bg-accent-700;
-  }
-
   .is-active-descendant {
-    @apply ring ring-accent-500 ring-opacity-50;
+    @apply bg-neutral-200 dark:bg-neutral-700;
   }
 </style>
