e636ef11cb345fbe8111a80caef69c9b0342d22a
ubermanu <manu@paca.happy-dev.fr>
2023-06-14 12:57:30 +0200

    feat: visual indicator on selected options

--- src/lib/components/Select/example/Option.svelte
+++ src/lib/components/Select/example/Option.svelte
@@ -4,15 +4,36 @@
 
   export let value: string
   export let selected: boolean = false
+  export let disabled: boolean = false
 
-  const { optionAttrs, select } = getContext<Select>('select')
+  const {
+    optionAttrs,
+    select,
+    selected: selectedList,
+  } = getContext<Select>('select')
 
   if (selected) select(value)
 </script>
 
 <div
   {...$optionAttrs(value)}
-  class="cursor-pointer rounded-md p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
+  class="flex cursor-pointer items-center justify-between rounded-md p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
 >
-  <slot />
+  <span>
+    <slot />
+  </span>
+  {#if $selectedList.includes(value)}
+    <svg
+      class="text-primary-500 dark:text-primary-400 ml-2 h-4 w-4"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      aria-hidden="true"
+    >
+      <path
+        clip-rule="evenodd"
+        d="M10.707 14.707a1 1 0 01-1.414 0L5 10.414A1 1 0 016.414 9l3.293 3.293 6.293-6.293a1 1 0 111.414 1.414l-7 7z"
+        fill-rule="evenodd"
+      />
+    </svg>
+  {/if}
 </div>
