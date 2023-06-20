ec14bf3827d36a08f41fecea5835d79014a1456a
ubermanu <manu@paca.happy-dev.fr>
2023-06-20 16:08:52 +0200

    make sidebar more compact

--- src/routes/(docs)/SidebarLink.svelte
+++ src/routes/(docs)/SidebarLink.svelte
@@ -7,7 +7,7 @@
 <a
   {href}
   aria-current={$page.url.pathname === href ? 'page' : undefined}
-  class="block w-full rounded-lg px-4 py-2 text-sm transition-colors duration-200 hover:bg-neutral-200 dark:hover:bg-neutral-800"
+  class="block w-full rounded-lg px-4 py-2 text-sm transition-colors duration-200 hover:bg-neutral-200 dark:hover:bg-neutral-800 md:px-2 md:py-1"
   class:is-active={$page.url.pathname === href}
 >
   <slot />
